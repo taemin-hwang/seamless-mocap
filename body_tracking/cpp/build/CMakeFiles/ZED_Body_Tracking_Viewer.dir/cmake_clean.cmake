ca2744f90f33260a069afeae1c4dd9b53be71d79
taemin <taemin.hwang@keti.re.kr>
2021-12-15 08:38:49 +0000

    Copy Zed-SDK's tutorial for body tracking

---
+++ body_tracking/cpp/build/CMakeFiles/ZED_Body_Tracking_Viewer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZED_Body_Tracking_Viewer.dir/src/GLViewer.o"
+  "CMakeFiles/ZED_Body_Tracking_Viewer.dir/src/TrackingViewer.o"
+  "CMakeFiles/ZED_Body_Tracking_Viewer.dir/src/main.o"
+  "ZED_Body_Tracking_Viewer.pdb"
+  "ZED_Body_Tracking_Viewer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZED_Body_Tracking_Viewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
