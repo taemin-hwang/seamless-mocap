d3c8ab3069065ba5491ad96b9900dd264ac230e0
taemin <taemin.hwang@keti.re.kr>
2021-12-23 07:17:47 +0000

    Enable reuse address in socket option

--- human_reconstructor/main.py
+++ human_reconstructor/main.py
@@ -1,23 +1,34 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import socketserver
+import socket
+import json
 
-class MyHTTPRequestHandler( BaseHTTPRequestHandler ):
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        print( 'Call GET request handler' )
+        print( 'GET requenst handler' )
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write('<h1>hello</h1>'.encode('utf-8'))
 
     def do_POST(self):
-        print( 'Call POST request handler' )
+        print( 'POST request handler' )
         content_len = int(self.headers.get('Content-Length'))
         post_body = self.rfile.read(content_len)
+        self.message = post_body
         print(post_body)
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
         output = ""
-        # must encode for python 3+
         self.wfile.write(output.encode())
 
-with socketserver.TCPServer(('127.0.0.1', 50001), MyHTTPRequestHandler) as httpd:
-  print('Server listening on port 50001...')
-  httpd.serve_forever()
+class MyTCPServer(socketserver.TCPServer):
+    def server_bind(self):
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind(self.server_address)
+
+httpd = MyTCPServer(('127.0.0.1', 50001), MyHTTPRequestHandler)
+print('Server listening on port 50001...')
+httpd.serve_forever()
