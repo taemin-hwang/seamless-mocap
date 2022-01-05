3825901f082e51779f7c9db6a4bc65f08039c39e
taemin <taemin.hwang@keti.re.kr>
2022-01-05 06:10:06 +0000

    Add 2D viewer for pose estimation

--- human_reconstructor/main.py
+++ human_reconstructor/main.py
@@ -1,15 +1,31 @@
+import sys
 import threading
 import time
+import json
 
 from transfer import skeleton_server
+from visualizer import viewer_2d
 
-skeleton_server.execute()
+'''
+  TEST CODE
+'''
+def run_test():
+    with open("./msg_2d.json", "r") as json_file:
+        item = json.load(json_file)
+        viewer_2d.render_2D(item)
 
-print('hello')
-while True:
-    mq = skeleton_server.message_queue
-    if mq.qsize() > 0:
-        item = mq.get()
-        print(item)
-    else:
-        time.sleep(0.01)
+def run():
+    skeleton_server.execute()
+    while True:
+        mq = skeleton_server.message_queue
+        if mq.qsize() > 0:
+            item = json.loads(mq.get())
+            #print(item)
+            viewer_2d.render_2D(item)
+        else:
+            time.sleep(0.01)
+
+if sys.argv.count('-t'):
+    run_test()
+else:
+    run()
---
+++ human_reconstructor/visualizer/viewer_2d.py
@@ -0,0 +1,66 @@
+#---------------------------------------------------
+#        2D VIEWER FOR POSE ESTIMATION
+#---------------------------------------------------
+
+import cv2
+import numpy as np
+from visualizer.utils import *
+
+def render_2D(data):
+    id = data['id']
+    timestamp = data['timestamp']
+    height = data['height']
+    width = data['width']
+    annots = data['annots']
+
+    display = np.zeros((height, width, 3), np.uint8)
+    color = generate_color_id_u(id)
+
+    for person in annots:
+        bbox = person['bbox']
+        person_id = person['personID']
+
+        cv2.rectangle(display, [bbox[0], bbox[1]], [bbox[2], bbox[3]], color)
+
+        keypoints = person['keypoints']
+        if len(keypoints) == 18:
+            print('keypoint format: 18')
+            # Draw skeleton bones
+            for part in SKELETON_BONES:
+                kp_a = keypoints[part[0].value]
+                kp_b = keypoints[part[1].value]
+                # Check that the keypoints are inside the image
+                if(kp_a[0] < display.shape[1] and kp_a[1] < display.shape[0]
+                and kp_b[0] < display.shape[1] and kp_b[1] < display.shape[0]
+                and kp_a[0] > 0 and kp_a[1] > 0 and kp_b[0] > 0 and kp_b[1] > 0 ):
+                    cv2.line(display, (int(kp_a[0]), int(kp_a[1])), (int(kp_b[0]), int(kp_b[1])), color, 1, cv2.LINE_AA)
+
+            # Get spine base coordinates to create backbone
+            left_hip = keypoints[BODY_PARTS.LEFT_HIP.value]
+            right_hip = keypoints[BODY_PARTS.RIGHT_HIP.value]
+            spine = [(left_hip[0] + right_hip[0])/2, (left_hip[1] + right_hip[1])/2]
+            kp_spine = spine
+            kp_neck = keypoints[BODY_PARTS.NECK.value]
+
+            # Check that the keypoints are inside the image
+            if(kp_spine[0] < display.shape[1] and kp_spine[1] < display.shape[0]
+            and kp_neck[0] < display.shape[1] and kp_neck[1] < display.shape[0]
+            and kp_spine[0] > 0 and kp_spine[1] > 0 and kp_neck[0] > 0 and kp_neck[1] > 0
+            and left_hip[0] > 0 and left_hip[1] > 0 and right_hip[0] > 0 and right_hip[1] > 0 ):
+                cv2.line(display, (int(kp_spine[0]), int(kp_spine[1])), (int(kp_neck[0]), int(kp_neck[1])), color, 1, cv2.LINE_AA)
+
+            # Skeleton joints for spine
+            if(kp_spine[0] < display.shape[1] and kp_spine[1] < display.shape[0]
+            and left_hip[0] > 0 and left_hip[1] > 0 and right_hip[0] > 0 and right_hip[1] > 0 ):
+                cv2.circle(display, (int(kp_spine[0]), int(kp_spine[1])), 3, color, -1)
+
+        elif len(keypoints) == 34:
+            print('keypoint format: 34')
+
+        for kp in keypoints:
+            if(kp[0] < display.shape[1] and kp[1] < display.shape[0]):
+                cv2.circle(display, (int(kp[0]), int(kp[1])), 3, color, -1)
+
+
+    cv2.imshow("2D Viewer", display)
+    cv2.waitKey(10)
