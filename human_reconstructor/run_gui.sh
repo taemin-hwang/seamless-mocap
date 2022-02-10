d2c8360bf5d11d4fbdccfbc2b17e6f42d0b52691
taemin <taemin.hwang@keti.re.kr>
2022-02-10 10:33:40 +0900

    Add scripts for displaying SMPL reconstruction

--- human_reconstructor/run_gui.sh
+++ human_reconstructor/run_gui.sh
@@ -1,4 +1,11 @@
-
 pushd easymocap
-python3 apps/vis/vis_server.py --cfg config/vis3d/o3d_scene.yml
+
+if [ $# -eq 0 ]; then
+  python3 apps/vis/vis_server.py --cfg config/vis3d/o3d_scene.yml
+else
+  if [ $1 == "-smpl" ]; then
+    python3 apps/vis/vis_server.py --cfg config/vis3d/o3d_scene_smpl.yml write True out ../output/mesh-smpl camera.cz 3. camera.cy 0.5
+  fi
+fi
+
 popd
