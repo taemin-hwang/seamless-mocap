60b7193e733a34d13b84c845d03b9effc66acdbe
taemin <taeminee@kakao.com>
2022-04-05 15:16:15 +0900

    Add mvmp option to run_gui.sh

--- human_reconstructor/run_gui.sh
+++ human_reconstructor/run_gui.sh
@@ -6,6 +6,9 @@ else
   if [ $1 == "-smpl" ]; then
     python3 apps/vis/vis_server.py --cfg config/vis3d/o3d_scene_smpl.yml write True out ../output/mesh-smpl camera.cz 3. camera.cy 0.5
   fi
+  if [ $1 == "-mvmp" ]; then
+    python3 apps/vis/vis_server.py --cfg config/vis3d/o3d_scene.yml write True out ../output/skel-multi camera.cz 3. camera.cy 0.5
+  fi
 fi
 
 popd
