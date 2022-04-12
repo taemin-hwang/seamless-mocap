e4ed21dba94500e6307b96af400e13d11bced26f
taemin <taeminee@kakao.com>
2022-04-12 17:56:28 +0900

    Add shell script for calibration

---
+++ calibrator/run1.sh
@@ -0,0 +1,6 @@
+extri=./extrinsic
+intri=./intrinsic
+
+mv ~/*.mp4 ./extrinsic/videos
+
+python3 ../human_reconstructor/easymocap/scripts/preprocess/extract_video.py ${extri} --no2d
---
+++ calibrator/run4.sh
@@ -0,0 +1,4 @@
+extri=./extrinsic
+intri=./intrinsic
+
+python3 ../human_reconstructor/easymocap/apps/calibration/calib_extri.py ${extri} --intri ${intri}/output/intri.yml
