b83dfec23b793c7e4d94c997fd2d7d0cd6e60585
taemin <taeminee@kakao.com>
2022-08-23 10:12:01 +0900

    Change websocket server url to plask url

--- animator/websocket-client/main.py
+++ animator/websocket-client/main.py
@@ -2,20 +2,29 @@
 
 import websocket
 import socket
+import time
+
+#from signal import signal, SIGPIPE, SIG_DFL
+#signal(SIGPIPE, SIG_DFL)
 
 udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 udp_socket.bind(('127.0.0.1', 7777))
 
 ws = websocket.WebSocket()
-uri = "ws://10.252.72.168:8000"
-ws.connect(uri)
+#url = "ws://10.252.72.168:8000"
+url = "wss://plask-keti.dot-sine.com/websocket:443"
+ws.connect(url)
 
+i = 0
 while(True):
+    print("RECEIVE MESSAGE FROM UNITY")
     bytes_address_pair = udp_socket.recvfrom(12288)
     message = bytes_address_pair[0]
     address = bytes_address_pair[1]
     client_message = message.decode('utf-8')
-    print(f"< {client_message}")
-    ws.send(client_message)
+    print("[SIZE: {}], {}".format(len(client_message), client_message))
+    print("SEND MESSAGE TO WSS SERVER")
+    ret = ws.send(client_message)
+    print(ret)
 
 ws.close()
