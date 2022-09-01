6af42305e2bd1ac2a95675f90be192aed4b17fc9
taemin <taeminee@kakao.com>
2022-09-01 16:57:28 +0900

    Change checkerboard size from 0.2 to 0.3

--- calibrator/2d/run2.sh
+++ calibrator/2d/run2.sh
@@ -1,4 +1,4 @@
 extri=./extrinsic
 intri=./intrinsic
 
-python3 ../../human_reconstructor/easymocap/apps/calibration/detect_chessboard.py ${extri} --out ${extri}/output/calibration --pattern 10,6 --grid 0.20
+python3 ../../human_reconstructor/easymocap/apps/calibration/detect_chessboard.py ${extri} --out ${extri}/output/calibration --pattern 10,6 --grid 0.30
