00ae8341cc38c84f31f7931f939e9a486bab9b7f
taemin <taeminee@kakao.com>
2023-04-04 12:52:04 +0900

    Make default skeleton detection model as zed-medium

--- skeleton_parser/py-skeleton-parser/main.py
+++ skeleton_parser/py-skeleton-parser/main.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser(description="Estimate 2D pose in real-time from RGB-D camera")
 
 parser.add_argument('-c', '--camera', default='zed', help='Camera model: zed, realsense')
-parser.add_argument('-m', '--model', required=True, help='Pre-trained model \n : zed-fast, zed-medium, zed-accurate, resnet, densnet, resnet-trt, denset-trt, higherhrnet-fast, higherhrnet-fast-trt, higherhrnet-medium, higherhrnet-accurate')
+parser.add_argument('-m', '--model', default='zed-medium', help='Pre-trained model \n : zed-fast, zed-medium, zed-accurate, resnet, densnet, resnet-trt, denset-trt, higherhrnet-fast, higherhrnet-fast-trt, higherhrnet-medium, higherhrnet-accurate')
 parser.add_argument('-r', '--resolution', default='HD1080', help='Image resolution from RGB camera: HD720, HD1080')
 parser.add_argument('-v', '--visual', action='store_true', help='Show 2D pose estimation results')
 parser.add_argument('-t', '--transfer', default=True, action='store_true', help='Transfer 2D pose estimation result')
